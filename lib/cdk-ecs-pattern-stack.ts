5ed1d007b1a54cb745ed681ea8617076bcf7d01e
Sovandara UOM <Sovandara.Uom@jtrustroyal.com>
2022-04-20 14:06:33 +0700

    Add fargate pattern

--- lib/cdk-ecs-pattern-stack.ts
+++ lib/cdk-ecs-pattern-stack.ts
@@ -1,16 +1,42 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Cluster } from 'aws-cdk-lib/aws-ecs';
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
+import { ContainerImage } from 'aws-cdk-lib/aws-ecs';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
 
 export class CdkEcsPatternStack extends Stack {
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'Vpc', { maxAzs: 2 });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEcsPatternQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new Cluster(this, 'Cluster', {
+      vpc: vpc,
+    });
+
+    const loadBalancedFargateService = new ApplicationLoadBalancedFargateService(this, 'Service', {
+      cluster,
+      memoryLimitMiB: 512,
+      desiredCount: 1,
+      cpu: 256,
+      taskImageOptions: {
+        image: ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      },
+    });
+
+    const scalableTarget = loadBalancedFargateService.service.autoScaleTaskCount({
+      minCapacity: 1,
+      maxCapacity: 2,
+    });
+
+    scalableTarget.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 80,
+    });
+
+    scalableTarget.scaleOnMemoryUtilization('MemoryScaling', {
+      targetUtilizationPercent: 80,
+    });
   }
 }
